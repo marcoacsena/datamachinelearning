09d23611034e178e08d767dd46555ca992f39bd2
marcoacsena <marcoacsena@gmail.com>
2025-02-07 09:44:28 -0300

    Standard eviation and percentile

--- standarddeviation.py
+++ standarddeviation.py
@@ -9,4 +9,9 @@ speed = [32,111,138,28,59,77,97]
 
 x = np.std(speed) #standard deviation high
 
-print(x)
+#print(x)
+
+#percentile
+ages = [5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
+p = np.percentile(ages, 90) #fórmula para calcular o percentil --> posição = (n, número de dados * p, percentil desejado em %) / 100
+print (p)
